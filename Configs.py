0b7b5e99d41422d1b9667ee0c2b9b5d752ed24aa
hyko-xd <96934642+hyko-xd@users.noreply.github.com>
2022-01-20 10:51:19 +0530

    Update Configs.py

--- Configs.py
+++ Configs.py
@@ -4,6 +4,6 @@ class Config(object):
     APP_ID = int(os.environ.get("APP_ID", 6))
     API_HASH = os.environ.get("API_HASH", None)
     TOKEN = os.environ.get("TOKEN", None)
-    BOT_US = os.environ.get("BOT_US", None)
+    BOT_US = os.environ.get("BOT_US", "")
     WELCOME_TEXT = os.environ.get("WELCOME_TEXT", None)
     RULES = os.environ.get("RULES", None)

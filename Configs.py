086ba0455a7a2c1eff5be4702e4fd49d8fe0e3ea
hyko-xd <96934642+hyko-xd@users.noreply.github.com>
2022-01-20 11:08:20 +0530

    Update Configs.py

--- Configs.py
+++ Configs.py
@@ -7,5 +7,4 @@ class Config(object):
     BOT_US = os.environ.get("BOT_US", "")
     WELCOME_TEXT = os.environ.get("WELCOME_TEXT", None)
     RULES = os.environ.get("RULES", None)
-    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
-    SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP", "")
+    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "BAZIGARXD")

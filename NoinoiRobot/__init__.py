2416bfbca011761c2469d69e9727470715d8ac57
hyko-xd <96934642+hyko-xd@users.noreply.github.com>
2022-01-20 10:12:59 +0530

    Delete __init__.py

--- NoinoiRobot/__init__.py
+++
@@ -1,9 +0,0 @@
-from telethon import TelegramClient
-import logging
-from Configs import Config
-
-logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
-                    level=logging.INFO)
-
-bot = TelegramClient('Stark', api_id=Config.APP_ID, api_hash=Config.API_HASH)
-Stark = bot.start(bot_token=Config.TOKEN)

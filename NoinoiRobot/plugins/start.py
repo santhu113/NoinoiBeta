b67721f5fc5cad5db5cd536f2f063c0d2ac49d71
hyko-xd <96934642+hyko-xd@users.noreply.github.com>
2022-01-20 10:32:01 +0530

    Update start.py

--- NoinoiRobot/plugins/start.py
+++ NoinoiRobot/plugins/start.py
@@ -2,7 +2,7 @@ from NoinoiRobot import Stark
 from telethon import events, Button
 
 PM_START_TEXT = """
-**Hey {}**
+**Hey {}** [.](https://telegra.ph/file/622a49ad89f050473b18a.jpg)
 I am simple manager robot whos help to you for manage your group
 
 **Click the below help button for the open help menu!**
@@ -14,7 +14,7 @@ async def start(event):
     if event.is_private:
        await event.reply(PM_START_TEXT.format(event.sender.first_name), buttons=[
         [Button.inline("Help & Commands", data="help")],
-        [Button.url("Source Code", "GitHub.com/HYKO-XD/NOINOIROBOT")]])
+        [Button.url("Source Code", "GitHub.com/HYKO-XD/NOINOIBETA")]])
        return
 
     if event.is_group:

57303dd828c9986b6a4b9124a80cba2cc906ed40
hyko-xd <96934642+hyko-xd@users.noreply.github.com>
2022-01-20 10:16:37 +0530

    Update utils.py

--- NoinoiRobot/utils.py
+++ NoinoiRobot/utils.py
@@ -4,11 +4,11 @@ import importlib
 from pathlib import Path
 
 def load_plugins(plugin_name):
-    path = Path(f"GPBot/plugins/{plugin_name}.py")
-    name = "GPBot.plugins.{}".format(plugin_name)
+    path = Path(f"NoinoiRobot/plugins/{plugin_name}.py")
+    name = "NoinoiRobot.plugins.{}".format(plugin_name)
     spec = importlib.util.spec_from_file_location(name, path)
     load = importlib.util.module_from_spec(spec)
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
-    sys.modules["GPBot.plugins." + plugin_name] = load
-    print("Bot has Started " + plugin_name)
+    sys.modules["NoinoiRobot.plugins." + plugin_name] = load
+    print("BOT HAS STARTED. . . . " + plugin_name)

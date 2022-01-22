4e674bee7fec7d588439952e76af3343be93cddd
santhu113 <95135053+santhu113@users.noreply.github.com>
2022-01-22 08:48:33 +0530

    Update start.py

--- NoinoiRobot/plugins/start.py
+++ NoinoiRobot/plugins/start.py
@@ -2,7 +2,7 @@ from NoinoiRobot import Stark
 from telethon import events, Button
 
 PM_START_TEXT = """
-**Hey {}** [.](https://telegra.ph/file/622a49ad89f050473b18a.jpg)
+**Hey {}** [.](https://te.legra.ph/file/e719f19bbeeb7f55e6202.jpg)
 I am simple manager robot whos help to you for manage your group
 
 **Click the below help button for the open help menu!**
@@ -14,9 +14,9 @@ async def start(event):
     if event.is_private:
        await event.reply(PM_START_TEXT.format(event.sender.first_name), buttons=[
         [Button.inline("Help & Commands", data="help")],
-        [Button.url("Source Code", "GitHub.com/HYKO-XD/NOINOIBETA")]])
+        [Button.url("OWNER 😁", "https://t.me/santhu_music_bot")]])
        return
 
     if event.is_group:
-       await event.reply("**I AM ALIVE ✨**")
+       await event.reply("**Nenu unna ✨**")
        return
